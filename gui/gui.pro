ab3223520c4737b102507296febe0ceca4073040
MaximSofronov <maxim.sofronov@gmail.com>
2018-03-28 20:29:38 +0300

    1 итерация
    Пользовательский интерфейс, главное окно программы.

---
+++ gui/gui.pro
@@ -0,0 +1,12 @@
+SOURCES += \
+    main.cpp \
+    form.cpp
+
+QT += widgets
+
+FORMS += \
+    form.ui \
+    dialog.ui
+
+HEADERS += \
+    form.h
---
+++ gui/main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include "form.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication application(argc, argv);
+
+    Form f(nullptr);
+    f.show();
+
+    return application.exec();
+}
